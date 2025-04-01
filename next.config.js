dfe4e31c2fa0f74dc341c4abbf91a9fba7157f8e
Christopher Burgos <burgoschris7@gmail.com>
2025-03-31 22:11:50 -0500

    Refactor next.config.js by removing the outputFileTracing setting to streamline configuration and prevent potential recursion issues.

--- next.config.js
+++ next.config.js
@@ -1,10 +1,7 @@
 const nextConfig = {
   experimental: {
-    optimizeCss: true,
-    esmExternals: 'loose'
-  },
-  // Disable file tracing completely since it's causing the stack overflow
-  outputFileTracing: false
+    optimizeCss: true
+  }
 };
 
 module.exports = nextConfig;
